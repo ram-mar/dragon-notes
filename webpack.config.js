5f8cb9904e33b889be6af9c9082967d779dfa72e
Ram Subramanian <ramsubramanian27@gmail.com>
2018-12-12 01:14:15 +0530

    Fixed webpack entry config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
-  entry: './src/client/index',
+  entry: './src/client/index.js',
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js'
